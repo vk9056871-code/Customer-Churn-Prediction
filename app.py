ce2ebc4b22547f3aaee3b677c18860fb0f53dce2
vikash kumar <vk9056871@gmail.com>
2026-01-31 20:37:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ with open('scaler.pkl', 'rb') as file:
 
 
 ## streamlit app
-st.title('Customer Churn PRediction')
+st.title('Customer Churn Prediction')
 
 # User input
 geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
@@ -68,3 +68,4 @@ if prediction_proba > 0.5:
     st.write('The customer is likely to churn.')
 else:
     st.write('The customer is not likely to churn.')
+
